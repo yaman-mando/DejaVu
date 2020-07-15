e0ef804588deaeb037736290f95b5aa84579194d
Adam <adamict.frontend@gmail.com>
2020-07-16 01:37:33 +0300

    create admin options enum

---
+++ src/app/enums/admin-options.enum.ts
@@ -0,0 +1,17 @@
+﻿
+/**** Menu interface ****/
+export interface AdminOption {
+  name: string;
+  link: string;
+  icon:string
+}
+
+/* Enum Of Items Menu*/
+export const AdminOptionsList = [
+  {name:'category', link:'admin',icon:'list'},
+  {name:'option 1', link:'admin',icon:'radio'},
+  {name:'option 2', link:'admin',icon:'video_settings'},
+  {name:'option 3', link:'admin',icon:'admin_panel_settings'},
+  {name:'option 4', link:'admin',icon:'settings'},
+];
+
