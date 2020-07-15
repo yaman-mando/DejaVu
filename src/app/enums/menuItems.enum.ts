49edcef750f42a395c85c6d8d7011932ecc78372
Adam <adamict.frontend@gmail.com>
2020-07-15 21:47:38 +0300

    Create menuItems enum file for menu Items

---
+++ src/app/enums/menuItems.enum.ts
@@ -0,0 +1,17 @@
+﻿
+/**** Menu interface ****/
+export interface Menu {
+  name: string;
+  link: string;
+  icon:string
+}
+
+/* Enum Of Items Menu*/
+export const MenuItems = [
+
+  {name:'Latest', link:'',icon:'refresh'},
+  {name:'home', link:'home',icon:'home'},
+  {name:'admin', link:'admin',icon:'admin_panel_settings'},
+  {name:'login', link:'login',icon:'login'},
+  {name:'logout', link:'',icon:'exit_to_app'},
+];
