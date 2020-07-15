5ea3a01a44066a0635dfc686520c09d638514d2e
Adam <adamict.frontend@gmail.com>
2020-07-15 21:50:20 +0300

    Create header component

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {ShareableService} from "../../services/shareable.service";
 
 @Component({
-  selector: 'app-header',
+  selector: 'appHeader',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _shareService:ShareableService) { }
 
   ngOnInit() {
   }
 
+  public toggleSidebar()
+  {
+    this._shareService.sidenavOpen = !this._shareService.sidenavOpen;
+  }
 }
