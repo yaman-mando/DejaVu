73f2f9ea3a55234be67e29c75ec4b108c628ac0c
Adam <adamict.frontend@gmail.com>
2020-07-17 01:46:26 +0300

    call loading component in general  app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {ShareableService} from "./services/shareable.service";
+import {TranslateService} from "@ngx-translate/core";
+import {AppStorageService, LangListStatic} from "./app-factory/services/app-storage.service";
+import {NavigationEnd, NavigationError, NavigationStart, Router} from "@angular/router";
+import {filter} from "rxjs/operators";
+import {HelpService} from "./services/help.service";
 
 @Component({
   selector: 'app-root',
@@ -9,7 +14,44 @@ import {ShareableService} from "./services/shareable.service";
 export class AppComponent {
   title = 'DejaVu';
 
-  constructor(public _shareService:ShareableService) {
+  constructor(private router: Router,
+              private _helpService:HelpService,
+              public _shareService: ShareableService,
+              private _appStorageService: AppStorageService,
+              private _translateService: TranslateService) {
+
+
+    if (this._translateService.currentLang === 'ar') {
+      document.documentElement.dir = 'rtl'
+    } else {
+      document.documentElement.dir = 'ltr'
+    }
+
+
+    this.router.events
+      .pipe(filter((res)=>{
+        return res instanceof NavigationStart
+      }))
+      .subscribe(async (res:NavigationStart)=>{
+        this._helpService.loading(true);
+      });
+    this.router.events
+      .pipe(filter((res) => {
+        return res instanceof NavigationEnd
+      }))
+      .subscribe(async (res: NavigationEnd) => {
+        this._helpService.loading(false);
+      });
+    this.router.events
+      .pipe(filter((res) => {
+        return res instanceof NavigationError
+      }))
+      .subscribe(async (res) => {
+        this._helpService.loading(false);
+      });
+
+
   }
+
 
 }
