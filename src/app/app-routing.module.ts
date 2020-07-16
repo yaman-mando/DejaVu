51c8f19f4015554cd38c50c3646d12ebd6e4d2ea
Adam <adamict.frontend@gmail.com>
2020-07-17 01:42:27 +0300

    Add admin & login pages to routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {AdminPanelComponent} from "./pages/admin-panel/admin-panel.component";
+import {LoginComponent} from "./pages/login/login.component";
 
 
 const routes: Routes = [
   {path:'home',component:HomeComponent},
-  {path:'',component:HomeComponent},
-  {
-    path:'**',
-    redirectTo:'home'
-  }
+  {path:'admin',component:AdminPanelComponent},
+  {path:'login',component:LoginComponent},
+  {path:'**', redirectTo:'home'}
   ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+
+export class AppRoutingModule {
+
+}
