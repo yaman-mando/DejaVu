b0b67e01857765a59c0a02ef3e585d0d33b5083a
Adam <adamict.frontend@gmail.com>
2020-07-17 01:33:06 +0300

    add arabic lists to our enums

--- src/app/enums/admin-options.enum.ts
+++ src/app/enums/admin-options.enum.ts
@@ -7,11 +7,19 @@ export interface AdminOption {
 }
 
 /* Enum Of Items Menu*/
-export const AdminOptionsList = [
+export const AdminOptionsList_EN = [
   {name:'category', link:'admin',icon:'list'},
   {name:'option 1', link:'admin',icon:'radio'},
   {name:'option 2', link:'admin',icon:'video_settings'},
   {name:'option 3', link:'admin',icon:'admin_panel_settings'},
   {name:'option 4', link:'admin',icon:'settings'},
+];
+
+export const AdminOptionsList_AR = [
+  {name:'التصنيفات', link:'admin',icon:'list'},
+  {name:'العنصر 1', link:'admin',icon:'radio'},
+  {name:'العنصر 2', link:'admin',icon:'video_settings'},
+  {name:'العنصر 3', link:'admin',icon:'admin_panel_settings'},
+  {name:'العنصر 4', link:'admin',icon:'settings'},
 ];
 
--- src/app/enums/menuItems.enum.ts
+++ src/app/enums/menuItems.enum.ts
@@ -6,13 +6,23 @@ export interface Menu {
   icon:string
 }
 
-/* Enum Of Items Menu*/
-export const MenuItems = [
+
+export const MenuItems_EN = [
 
   {name:'Latest', link:'',icon:'refresh'},
   {name:'home', link:'home',icon:'home'},
   {name:'admin', link:'admin',icon:'admin_panel_settings'},
   {name:'login', link:'login',icon:'login'},
   {name:'logout', link:'',icon:'exit_to_app'},
+];
+
+
+export const MenuItems_AR = [
+
+  {name:'الأحدث', link:'',icon:'refresh'},
+  {name:'الرئيسية', link:'home',icon:'home'},
+  {name:'لوحة المسؤول', link:'admin',icon:'admin_panel_settings'},
+  {name:'تسجيل الدخول', link:'login',icon:'login'},
+  {name:'تسجيل الخروج', link:'',icon:'exit_to_app'},
 ];
 
