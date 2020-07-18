bd57d68c47bc7a2deb897da482af75b559c0b7c0
Adam <adamict.frontend@gmail.com>
2020-07-18 23:07:06 +0300

    active login page for logins and verification

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,38 @@
+﻿import {NgModule} from "@angular/core";
+import {CommonModule} from "@angular/common";
+import {RouterModule, Routes} from "@angular/router";
+import {LoginComponent} from "./login.component";
+import {SignInFormComponent} from "../../components/sign-in-form/sign-in-form.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ReactiveFormsModule} from "@angular/forms";
+import {FlexModule} from "@angular/flex-layout";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatButtonModule} from "@angular/material/button";
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent,
+  }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    MatFormFieldModule,
+    ReactiveFormsModule,
+    FlexModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
+  ],
+  providers:[],
+  declarations:[LoginComponent,SignInFormComponent]
+})
+
+export class LoginModule {
+
+}
