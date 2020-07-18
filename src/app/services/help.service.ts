e50f1ad687722fd88fbcf76fc3602460c9af8a00
Adam <adamict.frontend@gmail.com>
2020-07-18 22:59:21 +0300

    create toast and current router services

--- src/app/services/help.service.ts
+++ src/app/services/help.service.ts
@@ -2,6 +2,8 @@
 import {AppStorageService, LangListStatic} from "../app-factory/services/app-storage.service";
 import {Router} from "@angular/router";
 import {BehaviorSubject, Observable} from "rxjs";
+import {TranslateBaseService} from "../app-translation/services/translation-base.service";
+import {ToastrService} from "ngx-toastr";
 
 
 @Injectable()
@@ -10,12 +12,21 @@ export class HelpService {
   private readonly loadingBeh:BehaviorSubject<boolean>;
   public readonly loading$:Observable<boolean>;
 
+  private readonly currentRouterBeh:BehaviorSubject<string>;
+  public readonly currentRouter$:Observable<string>;
+
+
 
   constructor(private _appStorageService:AppStorageService,
-              private _route:Router) {
+              private _route:Router,
+              private toastr: ToastrService,
+              private _tb:TranslateBaseService) {
 
     this.loadingBeh = new BehaviorSubject<boolean>(false);
     this.loading$ = this.loadingBeh.asObservable();
+
+    this.currentRouterBeh = new BehaviorSubject<string>(null);
+    this.currentRouter$ = this.currentRouterBeh.asObservable();
   }
 
   async changeLang(){
@@ -32,7 +43,7 @@ export class HelpService {
         langId:LangListStatic['en']
       });
     }
-    window.location.reload()
+    window.location.reload();
   }
 
   async logIn(_userName:string){
@@ -43,12 +54,12 @@ export class HelpService {
     await this._route.navigateByUrl('/home');
   }
 
-  async logOut(){
+  public async  logOut(){
     await this._appStorageService.setUserPreferences({
       isLoggedIn:false,
       userName:null,
     });
-    await this._route.navigateByUrl('/home');
+    await this._route.navigateByUrl('/login');
   }
 
   public loading(_case:boolean){
@@ -58,5 +69,39 @@ export class HelpService {
     else {
       this.loadingBeh.next(false);
     }
+  }
+
+  public setRouter(_url:string){
+    this.currentRouterBeh.next(_url);
+  }
+
+
+  /*show email password wrong toast*/
+  public async showEmailPasswordWrongToast(_duration: number = 1600) {
+    this.showToast(this._tb.translate.texts.check_email_password, _duration)
+  }
+
+  /* success toast */
+  public async showSuccessLogin(_duration: number = 1600){
+    this.showToastSuccess(this._tb.translate.texts.SIGNIN_SUCCESS, _duration)
+  }
+
+  /*show toast*/
+  public async showToast(_msg: string, _duration: number = 1600) {
+    await this.toastr.warning(_msg,'',{
+      closeButton: true,
+      progressBar: false,
+      positionClass: "toast-bottom-center",
+      timeOut: _duration,
+    });
+  }
+
+  public async showToastSuccess(_msg: string, _duration: number = 1600) {
+    await this.toastr.success(_msg,'',{
+      closeButton: true,
+      progressBar: false,
+      positionClass: "toast-bottom-center",
+      timeOut: _duration,
+    });
   }
 }
