f91e2bd5b1f7609fd7d89f2cdc13fe00064ca4b2
Adam <adamict.frontend@gmail.com>
2020-07-19 03:49:53 +0300

    create general json data and Ts interfaces for it

---
+++ src/app/data/general.data.ts
@@ -0,0 +1,61 @@
+﻿import {WI_CategoryCardList, WI_SlideShowList} from "../interfaces/general.interface";
+
+//slideShow list data
+export const slideShowList: WI_SlideShowList = {
+  slidesList: [
+    {
+      img: "../../assets/images/slider/h-slider-4.png",
+      title: "Image1 Title",
+      content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.",
+    },
+    {
+      img: "../../assets/images/slider/h-slider-3.jpg",
+      content: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam.",
+      title: "Image2 Title",
+    },
+  ]
+};
+
+
+//Category list data
+export const categoriesList: WI_CategoryCardList = {
+    categoriesList: [
+      {
+        img: "../../assets/images/categories/cat1.png",
+        title: "Image Title",
+        case_icon: 'add',
+        rate: 3
+      },
+      {
+        img: "../../assets/images/categories/cat2.png",
+        title: "Image Title",
+        case_icon: 'done',
+        rate: 2,
+      },
+      {
+        img: "../../assets/images/categories/cat3.png",
+        title: "Image Title",
+        case_icon: 'add',
+        rate: 3,
+      },
+      {
+        img: "../../assets/images/categories/cat4.png",
+        title: "Image Title",
+        case_icon: 'add',
+        rate: 5,
+      },
+      {
+        img: "../../assets/images/categories/cat5.png",
+        title: "Image Title",
+        case_icon: 'done',
+        rate: 3,
+      },
+      {
+        img: "../../assets/images/categories/cat6.png",
+        title: "Image Title",
+        case_icon: 'add',
+        rate: 4,
+      },
+    ]
+  }
+;
---
+++ src/app/interfaces/general.interface.ts
@@ -0,0 +1,25 @@
+﻿
+/*Category Card Interface*/
+export interface WI_CategoryCardObject{
+  img:string;
+  title:string;
+  case_icon:string;
+  rate:number
+}
+
+/*Category List Card Interface*/
+export interface WI_CategoryCardList {
+  categoriesList:WI_CategoryCardObject[]
+}
+
+/*Slider Object Interface*/
+export interface WI_SlideShowObject {
+  img:string;
+  title:string;
+  content:string
+}
+
+/*Slider List  Interface*/
+export interface WI_SlideShowList {
+  slidesList:WI_SlideShowObject[]
+}
