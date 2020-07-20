3f5556e1e2e683e4f73a00aeb70b50eb578d7754
Adam <adamict.frontend@gmail.com>
2020-07-20 06:00:18 +0300

    Begin building admin-panel page

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import {ShareableService} from "../../services/shareable.service";
+import {HelpService} from "../../services/help.service";
+import {sideNavMode} from "../../enums/general.enum";
+import {WI_AdminCatCardList, WI_CategoryCardList} from "../../interfaces/general.interface";
+import {adminCategoryCardsList} from "../../data/general.data";
 
 @Component({
   selector: 'app-admin-panel',
   templateUrl: './admin-panel.component.html',
-  styleUrls: ['./admin-panel.component.scss']
+  styleUrls: ['./admin-panel.component.scss'],
+  host: {
+    "(window:resize)":"this._helpService.checkSideBar()",
+    "(window:load)":"this._helpService.checkSideBar()",
+  }
 })
 export class AdminPanelComponent implements OnInit {
 
-  constructor() { }
+  adminCatCards:WI_AdminCatCardList;
+
+  constructor(public _shareService:ShareableService,
+              public _helpService:HelpService) {
+
+    /*getting data from source*/
+    this.adminCatCards = adminCategoryCardsList
+  }
 
   ngOnInit() {
+
   }
+
+
 
 }
--- src/app/pages/admin-panel/admin-panel.module.ts
+++ src/app/pages/admin-panel/admin-panel.module.ts
@@ -2,6 +2,23 @@
 import {CommonModule} from "@angular/common";
 import {AdminPanelComponent} from "./admin-panel.component";
 import {RouterModule, Routes} from "@angular/router";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {AdminSideBarComponent} from "../../components/adminComponents/admin-side-bar/admin-side-bar.component";
+import {MatListModule} from "@angular/material/list";
+import {MatIconModule} from "@angular/material/icon";
+import {PerfectScrollbarModule} from "ngx-perfect-scrollbar";
+import {AdminHeaderComponent} from "../../components/adminComponents/admin-header/admin-header.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {FormsModule} from "@angular/forms";
+import {MatMenuModule} from "@angular/material/menu";
+import {CategoryCardComponent} from "../../components/adminComponents/category-card/category-card.component";
+import {MatCardModule} from "@angular/material/card";
+import {MatTableModule} from "@angular/material/table";
 
 
 const routes: Routes = [
@@ -12,12 +29,26 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports:[
+  imports: [
     CommonModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule,
+    PerfectScrollbarModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatTooltipModule,
+    FormsModule,
+    MatMenuModule,
+    MatCardModule,
+    MatTableModule
   ],
   providers:[],
-  declarations:[AdminPanelComponent]
+  declarations:[AdminPanelComponent,AdminSideBarComponent,AdminHeaderComponent,CategoryCardComponent]
 })
 
 export class AdminPanelModule {
