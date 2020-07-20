e066cb7c164d439cc86ccd81814568e978883b5b
Adam <adamict.frontend@gmail.com>
2020-07-20 05:47:01 +0300

    use screen dimensions service in header component and change primary color to toolbar

--- src/app/components/generalComponents/header/header.component.ts
+++ src/app/components/generalComponents/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ShareableService} from "../../../services/shareable.service";
 import {TranslateBaseService} from "../../../app-translation/services/translation-base.service";
 import {AppStorageService} from "../../../app-factory/services/app-storage.service";
+import {HelpService} from "../../../services/help.service";
 
 
 @Component({
@@ -13,6 +14,7 @@ export class HeaderComponent implements OnInit {
 
 
   constructor(public _shareService:ShareableService,
+              public _help:HelpService,
               public _storage:AppStorageService,
               public _tb:TranslateBaseService) { }
 
