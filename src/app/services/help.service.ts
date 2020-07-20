697f99be33901700093530ae7acfe89aa5a7e1ca
Adam <adamict.frontend@gmail.com>
2020-07-20 05:48:32 +0300

    create ScreenDimensions && checkSideBar methods to help service

--- src/app/services/help.service.ts
+++ src/app/services/help.service.ts
@@ -4,20 +4,27 @@ import {Router} from "@angular/router";
 import {BehaviorSubject, Observable} from "rxjs";
 import {TranslateBaseService} from "../app-translation/services/translation-base.service";
 import {ToastrService} from "ngx-toastr";
+import {sideNavMode} from "../enums/general.enum";
+import {ShareableService} from "./shareable.service";
 
 
 @Injectable()
 export class HelpService {
 
+  /*for loading case in app */
   private readonly loadingBeh:BehaviorSubject<boolean>;
   public readonly loading$:Observable<boolean>;
 
+  /*for getting current router in app */
   private readonly currentRouterBeh:BehaviorSubject<string>;
   public readonly currentRouter$:Observable<string>;
 
-
+  /*for getting screen dimensions*/
+  screenWidth:number;
+  screenHeight:number;
 
   constructor(private _appStorageService:AppStorageService,
+              private _shareService:ShareableService,
               private _route:Router,
               private toastr: ToastrService,
               private _tb:TranslateBaseService) {
@@ -29,6 +36,8 @@ export class HelpService {
     this.currentRouter$ = this.currentRouterBeh.asObservable();
   }
 
+
+  /*change lang method*/
   async changeLang(){
     const currentLang = this._appStorageService.getUserPreferences().langId;
     if(currentLang == 1){
@@ -46,6 +55,7 @@ export class HelpService {
     window.location.reload();
   }
 
+  /*login processor*/
   async logIn(_userName:string){
     await this._appStorageService.setUserPreferences({
       isLoggedIn:true,
@@ -54,6 +64,7 @@ export class HelpService {
     await this._route.navigateByUrl('/home');
   }
 
+  /*logout processor*/
   public async  logOut(){
     await this._appStorageService.setUserPreferences({
       isLoggedIn:false,
@@ -62,6 +73,7 @@ export class HelpService {
     await this._route.navigateByUrl('/login');
   }
 
+  /*loading change case*/
   public loading(_case:boolean){
     if(_case){
       this.loadingBeh.next(true);
@@ -71,6 +83,7 @@ export class HelpService {
     }
   }
 
+  /*set current router in temp storage*/
   public setRouter(_url:string){
     this.currentRouterBeh.next(_url);
   }
@@ -81,12 +94,12 @@ export class HelpService {
     this.showToast(this._tb.translate.texts.check_email_password, _duration)
   }
 
-  /* success toast */
+  /* success login toast */
   public async showSuccessLogin(_duration: number = 1600){
     this.showToastSuccess(this._tb.translate.texts.SIGNIN_SUCCESS, _duration)
   }
 
-  /*show toast*/
+  /*general toast*/
   public async showToast(_msg: string, _duration: number = 1600) {
     await this.toastr.warning(_msg,'',{
       closeButton: true,
@@ -96,6 +109,7 @@ export class HelpService {
     });
   }
 
+  /*Success toast*/
   public async showToastSuccess(_msg: string, _duration: number = 1600) {
     await this.toastr.success(_msg,'',{
       closeButton: true,
@@ -104,4 +118,23 @@ export class HelpService {
       timeOut: _duration,
     });
   }
+
+  /*Screen Dimensions*/
+  public ScreenDimensions(){
+    this.screenWidth= window.innerWidth;
+    this.screenHeight=window.innerHeight;
+  }
+
+  /*check admin sideBar ang change case in sm devices*/
+  checkSideBar(){
+    if(this.screenWidth < 800){
+      this._shareService.adminSideBarMode = sideNavMode.over;
+      this._shareService.adminSideBarOpen=false;
+    }
+    else{
+      this._shareService.adminSideBarMode = sideNavMode.side;
+      this._shareService.adminSideBarOpen=true;
+    }
+  }
 }
+
