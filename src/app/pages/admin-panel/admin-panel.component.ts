0aa673f6dbd4d3703471d7c4f6ada29226ea1535
Adam <adamict.frontend@gmail.com>
2020-07-20 23:20:24 +0300

    add search & filter members & pagination to admin-panel page

--- src/app/pages/admin-panel/admin-panel.component.ts
+++ src/app/pages/admin-panel/admin-panel.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {ShareableService} from "../../services/shareable.service";
 import {HelpService} from "../../services/help.service";
-import {sideNavMode} from "../../enums/general.enum";
-import {WI_AdminCatCardList, WI_CategoryCardList} from "../../interfaces/general.interface";
+import {WI_AdminCatCardList} from "../../interfaces/general.interface";
 import {adminCategoryCardsList} from "../../data/general.data";
+import {TranslateBaseService} from "../../app-translation/services/translation-base.service";
+import {MatMenuTrigger} from "@angular/material/menu";
+import {MatCheckbox} from "@angular/material/checkbox";
 
 @Component({
   selector: 'app-admin-panel',
@@ -16,9 +18,17 @@ import {adminCategoryCardsList} from "../../data/general.data";
 })
 export class AdminPanelComponent implements OnInit {
 
+  @ViewChild(MatMenuTrigger,{static:false}) trigger: MatMenuTrigger;
+
+  @ViewChildren(MatCheckbox) allCheck:QueryList<MatCheckbox>;
+
   adminCatCards:WI_AdminCatCardList;
 
+  pageSize:number = 6;
+  activePage:number = 1;
+
   constructor(public _shareService:ShareableService,
+              public _tb:TranslateBaseService,
               public _helpService:HelpService) {
 
     /*getting data from source*/
@@ -29,6 +39,26 @@ export class AdminPanelComponent implements OnInit {
 
   }
 
+  /*close members menu Method*/
+  closeMatMenu(){
+    this.trigger.closeMenu()
+  }
 
+  /*check all choices Method*/
+  checkAllChoice(_value){
+    this.allCheck.map((res,i)=>{
+       if(i != 0) {
+         res.checked = !_value;
+       }
+     })
+  }
 
+  /*convert number to array of numbers method*/
+  numToArray(num:number):number[]{
+    let _arr = [];
+    for (let i=0;i<num;i++){
+      _arr.push(i);
+    }
+    return _arr
+  }
 }
