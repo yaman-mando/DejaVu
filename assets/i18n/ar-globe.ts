31d5d521dff44ee9e27d16c4d577388cb30a3752
Adam <adamict.frontend@gmail.com>
2020-07-21 03:09:57 +0300

    Auto-generated commit

---
+++ assets/i18n/ar-globe.ts
@@ -0,0 +1,47 @@
+/*import {langStatic_AR} from "../../app/enums/languages.enum";
+import {CurrencyStatic_AR} from "../../app/enums/currency.enum";
+import {CountryStatic_AR} from "../../app/enums/country.enum";
+import {GenderStatic_AR} from "../../app/enums/gender.enum";*/
+import {EN_GLOBE, EN_GLOBE_DATA} from "./en-globe";
+import {MenuItems_AR} from "../../app/enums/menuItems.enum";
+import {AdminOptionsList_AR} from "../../app/enums/admin-options.enum";
+
+
+
+export const AR_GLOBE_DATA: typeof EN_GLOBE_DATA = {
+    static: {
+      menuItems:MenuItems_AR,
+      adminList:AdminOptionsList_AR
+    },
+    page_title: {
+      home: 'الرئيسية',
+      admin: 'المسؤول',
+      login: 'تسجيل الدخول',
+    },
+  texts:{
+    lang:'en',
+    search:'بحث',
+    login_msg:'أدخل أي اسم مستخدم و كلمة مرور:123456 لتسجيل الدخول',
+    signIn_title:'تسجيل دخول المستخدم',
+    user_name:'اسم المستخدم',
+    password:'كلمة المرور',
+    signIn:'تسجيل دخول',
+    signOut:'تسجيل خروج',
+    check_email_password: 'يرجى التحقق من البريد الالكتروني او كلمة المرور',
+    SIGNIN_SUCCESS: "تم تسجيل الدخول بنجاح",
+    Management:'إدارة',
+    notifications:'الإشعارات',
+    category:'التصنيف',
+    changLang:'تغيير اللغة',
+    goToSite:'الذهاب إلى الموقع',
+    members:'الأعضاء',
+    allMembers:'جميع الأعضاء',
+    save:'حفظ'
+  }
+
+};
+
+export const AR_GLOBE: typeof EN_GLOBE = {
+  lang: 'ar',
+  data: AR_GLOBE_DATA,
+};
---
+++ assets/i18n/en-globe.ts
@@ -0,0 +1,47 @@
+/*import {CountryStatic_EN} from "../../app/enums/country.enum";
+import {GenderStatic_EN} from "../../app/enums/gender.enum";
+import {langStatic_EN} from "../../app/enums/languages.enum";
+import {CurrencyStatic_EN} from "../../app/enums/currency.enum";*/
+
+
+import {MenuItems_AR, MenuItems_EN} from "../../app/enums/menuItems.enum";
+import {AdminOptionsList_AR, AdminOptionsList_EN} from "../../app/enums/admin-options.enum";
+
+
+export const EN_GLOBE_DATA = {
+  static: {
+    menuItems:MenuItems_EN,
+    adminList:AdminOptionsList_EN
+  },
+  page_title: {
+    home: 'home',
+    admin: 'admin',
+    login: 'login',
+  },
+  texts:{
+    lang:'ar',
+    search:'Search',
+    login_msg:'Please Enter Any UserName and Password:123456 To Login',
+    signIn_title:'User Sign In',
+    user_name:'UserName',
+    password:'Password',
+    signIn:'Sign In',
+    signOut:'SignOut',
+    check_email_password: "please check your email and password.",
+    SIGNIN_SUCCESS: "Signin successfully",
+    Management:'MANAGEMENT',
+    notifications:'Notifications',
+    category:'Category',
+    changLang:'ChangeLang',
+    goToSite:'Go To Site',
+    members:'Members',
+    allMembers:'All Members',
+    save:'Save'
+  }
+
+};
+
+export const EN_GLOBE = {
+  lang: 'en',
+  data: EN_GLOBE_DATA,
+};
